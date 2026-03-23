45a980afda47b7ecadb3bd6b6d04c4c5994d0ea1
Enrico Tubigan <etubigan@academic.rrc.ca>
2026-03-23 16:47:41 -0500

    feat: add new route for retrieving all resources

--- src/api/v1/routes/projectRoutes.ts
+++ src/api/v1/routes/projectRoutes.ts
@@ -1,8 +1,10 @@
 import express, { Router } from "express";
 import * as resourceController from "../controllers/projectController";
+import { resolveCname } from "node:dns";
 
 const resourceRouter: express.Router = express.Router();
 
 resourceRouter.post("/", resourceController.createResourceHandler);
+resourceRouter.get("/", resourceController.getAllItemsHandler);
 
 export default resourceRouter;
