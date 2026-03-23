0f2669e30e6bc947ca9137d23dd8836d44a4f449
Enrico Tubigan <etubigan@academic.rrc.ca>
2026-03-23 16:17:07 -0500

    feat: create controller function for handling request and response for creating resource

---
+++ src/api/v1/controllers/projectController.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import * as resourceServices from "../services/projectService";
+import { successResponse, errorResponse } from "../models/responseModel";
+import { HTTP_STATUS } from "../../../constants/httpConstants";
+
+export const createResourceHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+): Promise<void> => {
+    try {
+        const { title, type, url, description } = req.body;
+        const data = { title, type, url, description };
+
+        // Call service function.
+        const resource = await resourceServices.createResource(data);
+
+        res.status(HTTP_STATUS.CREATED).json(
+            successResponse(resource, "Resource successfully created") // Use successful response model.
+        );
+    } catch (error: unknown) {
+        next(error);
+    }
+};
