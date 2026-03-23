5d18ddc3f761a510e06f119091740e1034741d92
Enrico Tubigan <etubigan@academic.rrc.ca>
2026-03-23 17:21:53 -0500

    fix: fix route with id

--- src/api/v1/routes/projectRoutes.ts
+++ src/api/v1/routes/projectRoutes.ts
@@ -54,7 +54,7 @@ const resourceRouter: express.Router = express.Router();
 resourceRouter.post("/", validateRequest(itemSchemas.create), resourceController.createResourceHandler);
 
 resourceRouter.get("/", resourceController.getAllItemsHandler);
-resourceRouter.get("/{id}", validateRequest(itemSchemas.getById), resourceController.getItemByIdHandler);
-resourceRouter.delete("/{id}", validateRequest(itemSchemas.getById), resourceController.deleteItemHandler);
+resourceRouter.get("/:id", validateRequest(itemSchemas.getById), resourceController.getItemByIdHandler);
+resourceRouter.delete("/:id", validateRequest(itemSchemas.getById), resourceController.deleteItemHandler);
 
 export default resourceRouter;
